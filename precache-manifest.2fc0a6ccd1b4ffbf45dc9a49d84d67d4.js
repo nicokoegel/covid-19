4e4f13dc3ab42345af6709b2ddb0a8f4843f3f6b
nicokoegel <koegel99@gmail.com>
2020-05-12 19:23:09 +0200

    Updates

---
+++ precache-manifest.2fc0a6ccd1b4ffbf45dc9a49d84d67d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a944aa2adec1b37815d81fd9c94677a",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "419b2ce04e6814bad872",
+    "url": "/covid-19/static/css/main.e88c6d94.chunk.css"
+  },
+  {
+    "revision": "ad7b2b2d57eb23cf8cbd",
+    "url": "/covid-19/static/js/2.5d122808.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19/static/js/2.5d122808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419b2ce04e6814bad872",
+    "url": "/covid-19/static/js/main.7402886a.chunk.js"
+  },
+  {
+    "revision": "c59e2f922549c307064d",
+    "url": "/covid-19/static/js/runtime-main.aabceb11.js"
+  }
+]);
